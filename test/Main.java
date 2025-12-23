51ffb0436326f4cacd910ce92da05305f4ed5880
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-12-23 19:06:37 +0600

    environment setup done for java

---
+++ test/Main.java
@@ -0,0 +1,17 @@
+package test;
+
+// public class kk {
+//     public static void main() {
+//         System.out.println("kamrujjaman al kudrot");
+//     }
+// }
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("MD Kamrujjaman");
+
+    }
+}
+
